4e0a951d5328106ce457c255d04c536cd2ba0edc
purnimasingh <purnima.singh@shiprocket.com>
2023-01-06 16:06:20 +0530

    splash

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "431d9c952ebce50ed5dd7a3fb0f7f42c",
-"index.html": "b0fe92539096692f13393edcf8bb6227",
-"/": "b0fe92539096692f13393edcf8bb6227",
+"index.html": "1661332db077a4b252fecca5b04fb89e",
+"/": "1661332db077a4b252fecca5b04fb89e",
 "styles.css": "01ddf5811b8593a3d721ede630e064d4",
 "main.dart.js": "af8c22c9b9fc9d2d04cbb257380c2e78",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
